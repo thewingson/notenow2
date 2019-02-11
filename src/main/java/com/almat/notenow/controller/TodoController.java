5fd65689b82fde8172ca3a2a9f1666e4dfbaecb7
thewingson <almatrakh97@gmail.com>
2019-02-11 15:16:42 +0600

    First commit and push.
    Files:
    TodoController.java - basic controller
    NotFoundException.java - api request exception

---
+++ src/main/java/com/almat/notenow/controller/TodoController.java
@@ -0,0 +1,65 @@
+package com.almat.notenow.controller;
+
+import com.almat.notenow.exception.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+    private int count = 4;
+    private List<Map<String, String>> todos = new ArrayList<Map<String, String>>(){{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "lingard"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "rasford"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "martial"); }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list(){
+        return todos;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id){
+        return getTodo(id);
+    }
+
+    private Map<String, String> getTodo(@PathVariable String id) {
+        return todos.stream()
+                .filter(todos -> todos.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> todo){
+        todo.put("id", String.valueOf(count++));
+
+        todos.add(todo);
+
+        return todo;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> todo){
+        Map<String, String> todoFromDb = getTodo(id);
+
+        todoFromDb.putAll(todo);
+
+        todoFromDb.put("id", id);
+
+        return todoFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Map<String, String> todo = getTodo(id);
+
+        todos.remove(todo);
+    }
+
+}
